89dfa982d6458b57a1c2a7368c779a9fbf781b89
harry <harryspitsillides@hotmail.com>
2018-04-11 12:37:46 +0300

    Allow user to add comment and rating for source

--- app/src/main/java/com/example/harry/mynews/ViewModel/RateSourceViewModel.java
+++ app/src/main/java/com/example/harry/mynews/ViewModel/RateSourceViewModel.java
@@ -1,9 +1,17 @@
 package com.example.harry.mynews.ViewModel;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.TabLayout;
+import android.util.Log;
+
 import com.example.harry.mynews.Model.Firebase.RatingModel;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Exclude;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.List;
@@ -13,8 +21,10 @@ public class RateSourceViewModel {
     private static final String TAG = UserViewModel.class.getSimpleName();
     private FirebaseUser user;
     private DatabaseReference databaseReference;
+
     private final String SOURCE_PROVIDERS = "sourceProviders";
-    private final String REVIEWS = "reviews";
+    private final String REVIEWS = "value";
+    private final String USERS = "users";
 
 
     public RateSourceViewModel(FirebaseUser user, DatabaseReference databaseReference) {
@@ -25,31 +35,46 @@ public class RateSourceViewModel {
     public void updateSourceProviderRating(String providerId, float rating, String comment) {
         RatingModel model = new RatingModel(comment, user.getDisplayName(), rating, user.getUid());
         databaseReference.child(SOURCE_PROVIDERS).child(providerId).push().setValue(model); // push so that it wont overwrite node
+        databaseReference.child(SOURCE_PROVIDERS).child(providerId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                dataSnapshot.getChildren().forEach(child -> {
+                    RatingModel model = child.getValue(RatingModel.class);
+                    Log.d(TAG, model.toString());
+                });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
 
 class ReviewsHolder {
-    private List<RatingModel> reviews;
+    private List<RatingModel> value;
 
     public ReviewsHolder() {
     }
 
-    public ReviewsHolder(List<RatingModel> reviews) {
-        this.reviews = reviews;
+    public ReviewsHolder(List<RatingModel> value) {
+        this.value = value;
     }
 
-    public List<RatingModel> getReviews() {
-        return reviews;
+    public List<RatingModel> getValue() {
+        return value;
     }
 
-    public void setReviews(List<RatingModel> reviews) {
-        this.reviews = reviews;
+    public void setValue(List<RatingModel> value) {
+        this.value = value;
     }
 
     @Exclude
     public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
-        result.put("reviews", reviews);
+        result.put("value", value);
 
         return result;
     }
